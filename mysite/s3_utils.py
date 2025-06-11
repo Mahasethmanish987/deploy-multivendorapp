fcafa8a1654653307b3ba89388b51a255c9958f1
manish <mahasethmanish63@gmail.com>
2025-06-12 03:43:36 +0545

    hdkl;jdsk

--- mysite/s3_utils.py
+++ mysite/s3_utils.py
@@ -1,31 +1,18 @@
-from storages.backends.s3boto3 import S3Boto3Storage
+# mysite/s3_utils.py
 from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
     file_overwrite = False
-    default_acl = 'public-read'
-    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
-    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
-    region_name = settings.AWS_S3_REGION_NAME
-
-    def __init__(self, *args, **kwargs):
-        kwargs['access_key'] = settings.AWS_ACCESS_KEY_ID
-        kwargs['secret_key'] = settings.AWS_SECRET_ACCESS_KEY
-        super().__init__(*args, **kwargs)
+    
+    @property
+    def bucket_name(self):
+        return settings.AWS_STORAGE_BUCKET_NAME
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
-    default_acl = 'public-read'
-    file_overwrite = True
-    gzip = True
-    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
-    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
-    region_name = settings.AWS_S3_REGION_NAME
-    querystring_auth = False
-    headers = {'Cache-Control': 'max-age=31536000, public, immutable'}
-
-    def __init__(self, *args, **kwargs):
-        kwargs['access_key'] = settings.AWS_ACCESS_KEY_ID
-        kwargs['secret_key'] = settings.AWS_SECRET_ACCESS_KEY
-        super().__init__(*args, **kwargs)
+    
+    @property
+    def bucket_name(self):
+        return settings.AWS_STORAGE_BUCKET_NAME
