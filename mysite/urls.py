c466c6f8a8ee3c9e0e1c4c9ef3c5eb070302ecd3
manish <mahasethmanish63@gmail.com>
2026-06-05 22:58:24 +0545

    check url loaded

--- mysite/urls.py
+++ mysite/urls.py
@@ -32,3 +32,4 @@ urlpatterns = [
     path('order/',include('order.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+print("URLS LOADED")
